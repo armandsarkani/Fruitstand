4d6225ea81add7524e823f8408d3236ef1a6b8e7
armandsarkani <armandsarkani@icloud.com>
2021-05-17 12:16:04 -0700

    Initial Commit

---
+++ Fruitstand/FruitstandApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitstandApp.swift
+//  Fruitstand
+//
+//  Created by Armand Sarkani on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitstandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
